7bfca31f6e20bae0b8afe2155ab99726e0e18a5d
Pabllot07 <pabllot07@outlook.com>
2023-01-31 18:31:08 -0300

    cleaning form

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Quantity from '../Quantity/Quantity';
 import { Button, CloseButton, Desc, Image, MoreDetails, Name, Price, Title, Wrapper } from './styles'
 
@@ -15,9 +15,7 @@ interface Props {
 
 const Card = ({image, title, Mprice, XGprice, Gprice, desc, id}: Props) => {
     const [moreDetails, setMoreDetails] = useState(false)
-    const [pizzaSize, setPizzaSize] = useState(Mprice)
     const [total, setTotal] = useState(1)
-    const [name, setName] = useState('')
     const [form, setForm] = useState({
         name: '',
         pizzaId: id,
@@ -51,17 +49,17 @@ const Card = ({image, title, Mprice, XGprice, Gprice, desc, id}: Props) => {
             <Title >{title}</Title>
             <Desc>{desc}</Desc>
             {!moreDetails && <Button onClick={()=>setMoreDetails(true)}>Ver mais</Button>}
-    {moreDetails && 
-        <MoreDetails>
-            <Price onClick={()=>handlePrice(Mprice)}>M R$ {Mprice},00</Price>
-            <Price onClick={()=>handlePrice(Gprice)}>G R$ {Gprice},00</Price>
-            <Price onClick={()=>handlePrice(XGprice)}>XG R$ {XGprice},00</Price>
-            <Quantity total={total} setTotal={setTotal} handleQuantity={handleQuantity} />
-            <Name placeholder='Nome' name='name' type='text' defaultValue={form.name} onChange={handleChange}/>
-            <Button onClick={handleSubmit}>Confirmar</Button>
-            <CloseButton onClick={()=>setMoreDetails(false)}>X</CloseButton>
-        </MoreDetails>
-    }
+          {moreDetails && 
+              <MoreDetails>
+                  <Price onClick={()=>handlePrice(Mprice)}>M R$ {Mprice},00</Price>
+                  <Price onClick={()=>handlePrice(Gprice)}>G R$ {Gprice},00</Price>
+                  <Price onClick={()=>handlePrice(XGprice)}>XG R$ {XGprice},00</Price>
+                  <Quantity total={total} setTotal={setTotal} handleQuantity={handleQuantity} />
+                  <Name placeholder='Nome' name='name' type='text' defaultValue={form.name} onChange={handleChange}/>
+                  <Button onClick={handleSubmit}>Confirmar</Button>
+                  <CloseButton onClick={()=>setMoreDetails(false)}>X</CloseButton>
+              </MoreDetails>
+          }
         </Wrapper>
        
     </>
