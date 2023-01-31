d8250631f3860d03a183fd1a68f1de30c1ae5a6b
Pabllot07 <pabllot07@outlook.com>
2023-01-30 23:33:38 -0300

    Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home/Home'
 import { Container } from './styles'
 
 const App = () => {
   return (
     <Container>
       <Navbar />
+      <Home />
     </Container>
   )
 }
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    position: relative;
+    margin-top: 10vh;
+    height: 90vh;
     
 `
