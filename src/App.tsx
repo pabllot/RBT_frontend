683f09fcc4e7203f2daefd5a997d5dbf59e784c7
Pabllot07 <pabllot07@outlook.com>
2023-02-01 15:00:17 -0300

    routes implementend

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import Navbar from './components/Navbar/Navbar'
+import Admin from './pages/Admin/Admin'
 import Home from './pages/Home/Home'
 import { Container } from './styles'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <Container>
-      <Navbar />
-      <Home />
-    </Container>
+    <BrowserRouter>
+      <Container>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/admin' element={<Admin />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   )
 }
 
